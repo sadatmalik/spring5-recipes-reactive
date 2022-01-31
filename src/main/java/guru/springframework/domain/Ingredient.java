99749b43677804ab0336327f669c4ec38934f2cf
sadat.malik <sadat.malik@me.com>
2022-01-31 15:20:05 +0000

    Setting up Reactive Repositories. Needs further refactoring as model classes have been migrated to @Document so won't work with old JPA repositories any longer. Closes #3

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -2,10 +2,10 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * Created by jt on 6/13/17.
@@ -14,8 +14,7 @@ import java.math.BigDecimal;
 @Setter
 public class Ingredient {
 
-    @Id
-    private String id;
+    private String id = UUID.randomUUID().toString();
     private String description;
     private BigDecimal amount;
 
@@ -23,6 +22,7 @@ public class Ingredient {
     private UnitOfMeasure uom;
 
     public Ingredient() {
+
     }
 
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
@@ -35,7 +35,6 @@ public class Ingredient {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
-       // this.recipe = recipe;
     }
 
 }
--- src/main/java/guru/springframework/services/IngredientServiceImpl.java
+++ src/main/java/guru/springframework/services/IngredientServiceImpl.java
@@ -9,7 +9,6 @@ import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -55,11 +54,14 @@ public class IngredientServiceImpl implements IngredientService {
             log.error("Ingredient id not found: " + ingredientId);
         }
 
+        //enhance command object with recipe id
+        IngredientCommand ingredientCommand = ingredientCommandOptional.get();
+        ingredientCommand.setRecipeId(recipe.getId());
+
         return ingredientCommandOptional.get();
     }
 
     @Override
-    @Transactional
     public IngredientCommand saveIngredientCommand(IngredientCommand command) {
         Optional<Recipe> recipeOptional = recipeRepository.findById(command.getRecipeId());
 
@@ -107,8 +109,13 @@ public class IngredientServiceImpl implements IngredientService {
                         .findFirst();
             }
 
-            //to do check for fail
-            return ingredientToIngredientCommand.convert(savedIngredientOptional.get());
+            //todo check for fail
+
+            //enhance with id value
+            IngredientCommand ingredientCommandSaved = ingredientToIngredientCommand.convert(savedIngredientOptional.get());
+            ingredientCommandSaved.setRecipeId(recipe.getId());
+
+            return ingredientCommandSaved;
         }
 
     }
